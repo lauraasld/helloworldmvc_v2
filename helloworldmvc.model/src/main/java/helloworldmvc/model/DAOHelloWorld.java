1ac78b2564b80db6952054c8155507574574bb51
lauraasld <laura.schlauder@viacesi.fr>
2017-06-07 13:27:43 +0200

    phase 4 v2

--- helloworldmvc.model/src/main/java/helloworldmvc/model/DAOHelloWorld.java
+++ helloworldmvc.model/src/main/java/helloworldmvc/model/DAOHelloWorld.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class DAOHelloWorld {
-	private static String			FileName			= "helloWorld.txt";
+	private static String			fileName			= "helloWorld";
 	private static DAOHelloWorld	instance			= null;
 	private String					helloWorldMessage	= null;
 
@@ -15,7 +15,7 @@ public class DAOHelloWorld {
 	private void readFile() {
 		BufferedReader bufferstock = null;
 		try {
-			bufferstock = new BufferedReader(new FileReader(DAOHelloWorld.FileName));
+			bufferstock = new BufferedReader(new FileReader(DAOHelloWorld.fileName));
 			StringBuilder stringbuilder = new StringBuilder();
 			this.helloWorldMessage = bufferstock.readLine();
 		} catch (Exception e) {
